507a6f2d6c54944978f3da5e8626e4eb7038fe92
Adam <61081143+Adam1141@users.noreply.github.com>
2022-02-23 22:35:17 +0200

    finished about page

--- view/pages/about.php
+++ view/pages/about.php
@@ -5,4 +5,15 @@
     <img class="page-bg" src="/url-shortener/view/img/about-bg.svg#svgView(preserveAspectRatio(none))" alt="wavy background image">
 
     <h2>About</h2>
+    <h3>Shortiii</h3>
+    <p>
+        Shortiii is an easy to use url shortener, it is meant to help developers and campaign managers to shorten long urls to make them more elegant, appealing and most importantly SHORT.
+    </p>
+    <h3>URL Shortening</h3>
+    <p>
+        URL shortening is a technique on the World Wide Web in which a Uniform Resource Locator (URL) may be made substantially shorter and still direct to the required page. This is achieved by using a redirect which links to the web page that has a long URL. For example, the URL "https://example.com/assets/category_B/subcategory_C/Foo/" can be shortened to "https://example.com/Foo", and the URL "https://en.wikipedia.org/wiki/URL_shortening" can be shortened to "https://w.wiki/U". Often the redirect domain name is shorter than the original one. A friendly URL may be desired for messaging technologies that limit the number of characters in a message (for example SMS), for reducing the amount of typing required if the reader is copying a URL from a print source, for making it easier for a person to remember, or for the intention of a permalink. In November 2009, the shortened links of the URL shortening service Bitly were accessed 2.1 billion times.
+    </p>
+    <h3>Advantages</h3>
+    <p>The main advantage of a short link is that it is, as the name indicates, short, looks neat and clean and can be more easily communicated and entered without error. To a very limited extent it may obscure the destination of the URL, though easily discoverable; this may be advantageous, disadvantageous, or irrelevant. A short link which expires, or can be terminated, has some security advantages.</p>
+
 </section>
